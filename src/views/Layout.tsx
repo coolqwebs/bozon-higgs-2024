a28b3536ae49ef512585e7f038c6a2825b7f4a29
coolqwebs <asankadyrovnk@gmail.com>
2024-09-27 23:59:18 +0600

    bg

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -16,9 +16,9 @@ const Layout = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuth]);
-
+  // bg-[url('/noise1.gif')]
   return (
-    <div className="w-svw h-svh flex flex-col items-center gap-10 py-5 font-mono bg-[url('/noise1.gif')] bg-contain">
+    <div className="w-svw h-svh flex flex-col items-center gap-10 py-5 font-mono">
       <Header />
       <main className="w-[80%] h-[80%] flex flex-col gap-5 items-start justify-center">
         <Outlet />
