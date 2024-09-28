ec259527255b4fb2525ac3cf02a37617eb6fdf3a
coolqwebs <asankadyrovnk@gmail.com>
2024-09-28 14:54:18 +0600

    layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,16 +29,10 @@ const Header = () => {
   };
 
   return (
-    <header className="w-[80%] px-12 py-2 border-[2px] border-gray-500 dark:border-slate-300 rounded-xl backdrop-blur-xl flex flex-wrap items-center justify-between">
+    <header className="w-full px-12 py-2 border-[2px] border-gray-500 dark:border-slate-300 rounded-xl backdrop-blur-xl flex flex-wrap items-center justify-between">
       <div className="flex items-center justify-center">
         <Link to="/">
-          <img
-            src={Logo}
-            alt=""
-            className="logo md:-mb-3 lg:-mb-4 xl:-mb-6"
-            // width="400"
-            // height="20"
-          />
+          <img src={Logo} alt="" className="logo md:-mb-3 lg:-mb-4 xl:-mb-6" />
         </Link>
       </div>
       <Timer />
@@ -56,58 +50,65 @@ const Header = () => {
           <li className="text-2xl hidden 2xl:block">
             <Link to="/faq">FAQ</Link>
           </li>
-
-          {!isAuth ? (
-            <li className="text-2xl">
+          {!isAuth && (
+            <li className="text-2xl hidden 2xl:block">
               <Link to="/login">Sign in</Link>
             </li>
-          ) : (
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar>
-                  <AvatarImage src="/logo.ico" className="animate-spin" />
-                  <AvatarFallback>{user?.userName[0]}</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end">
+          )}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="/logo.ico" className="animate-spin" />
+                <AvatarFallback>{user?.userName[0]}</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end">
+              {isAuth ? (
                 <DropdownMenuItem>
                   <Link className="text-2xl w-full" to="/profile">
                     Profile
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Link
-                    className="text-2xl w-full block 2xl:hidden "
-                    to="/scoreboard"
-                  >
-                    Scoreboard
+              ) : (
+                <DropdownMenuItem className="block 2xl:hidden">
+                  <Link className="text-2xl w-full" to="/login">
+                    Sign in
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Link
-                    className="text-2xl w-full block 2xl:hidden "
-                    to="/challenges"
-                  >
+              )}
+
+              <DropdownMenuItem className="block 2xl:hidden">
+                <Link className="text-2xl w-full" to="/scoreboard">
+                  Scoreboard
+                </Link>
+              </DropdownMenuItem>
+              {isAuth && (
+                <DropdownMenuItem className="block 2xl:hidden">
+                  <Link className="text-2xl w-full " to="/challenges">
                     Challenges
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Link className="text-2xl w-full block 2xl:hidden " to="/faq">
-                    FAQ
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <p
-                    className="w-full text-2xl cursor-pointer"
-                    onClick={logoutHandler}
-                  >
-                    Log out
-                  </p>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
+              )}
+              <DropdownMenuItem className="block 2xl:hidden">
+                <Link className="text-2xl w-full" to="/faq">
+                  FAQ
+                </Link>
+              </DropdownMenuItem>
+              {isAuth && (
+                <>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <p
+                      className="w-full text-2xl cursor-pointer"
+                      onClick={logoutHandler}
+                    >
+                      Log out
+                    </p>
+                  </DropdownMenuItem>
+                </>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </ul>
       </nav>
     </header>
